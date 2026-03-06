ebab343439920aec3b55c24c86830dc734b86694
Petros607 <markaryan.petr@mail.ru>
2026-03-06 16:35:16 +0400

    init

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+# app/__init__.py
+from app import models
+from app import schemas
+from app import database
+from app import routers
---
+++ app/main.py
@@ -0,0 +1,14 @@
+# app/main.py
+from fastapi import FastAPI
+# Импортируем наш новый модуль с роутером
+from app.routers import users
+
+app = FastAPI(title="My Architecture App")
+
+# Подключаем роутер к главному приложению.
+# Это похоже на подключение плагина.
+app.include_router(users.router)
+
+@app.get("/")
+def root():
+    return {"message": "Приложение работает!"}
